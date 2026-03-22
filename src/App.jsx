8e3a8ef0ba8d431fab3a415869d01732f6daccb1
Sachzie <schz.ilagan@gmail.com>
2026-03-22 22:05:16 +0800

    anpther polishing

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,7 @@ const moduleData = {
  
  Finally, the presentation covers the applications of CMS, showing how it is used in website publishing, e-commerce, digital marketing, corporate portals, educational platforms, and multi-platform content distribution. This highlights the importance of CMS in modern digital communication and online business operations.`,
     pptUrl: "https://docs.google.com/presentation/d/1EKeBkwX7etZlKCzzlAlm4HeEYvkE1lkG/edit",
-    videoUrl: ["https://youtu.be/qVnLrvShvSc", "https://youtube.com/watch?v=0BnUU7l7nXc&si=sKI7lO4lV-sMjdIx"],
+    videoUrl: ["https://www.youtube.com/watch?v=qVnLrvShvSc&feature=youtu.be&fbclid=IwY2xjawQs1kZleHRuA2FlbQIxMABicmlkETFQcThhUzBianFGVmFTMExMc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHiQUF6vAOPB5mCcc1lTK5i7GuDdiX8eX0q2SXVxvjcTtVgA4czCbK2ckI16k_aem_IqAywZ5ZKdE0G7p3WXgX3w", "https://youtube.com/watch?v=0BnUU7l7nXc&si=sKI7lO4lV-sMjdIx"],
     members: [
       { name: 'DANQUE, JOHN MICHAEL', role: 'Member' },
       { name: 'CIPRIANO, YHANSKIE ADRIEL', role: 'Member' },
